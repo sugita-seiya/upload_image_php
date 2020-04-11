dc65a5fbbaea8d6375a14b0f883af6d7b3c3a32e
sugita-seiya <satreu16@ezweb.ne.jp>
2020-04-11 10:45:43 +0900

    画像投稿機能の実装

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 
+ini_set('display_errors', 1);
+define('MAX_FILE_SIZE', 1 * 1024 * 1024); // 1MB
+define('THUMBNAIL_WIDTH', 400);
+define('IMAGES_DIR', __DIR__ . '/images');
+define('THUMBNAIL_DIR', __DIR__ . '/thumbs');
+
+if (!function_exists('imagecreatetruecolor')) {
+  echo 'GD not installed';
+  exit;
+}
+
+function h($s) {
+  return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+}
+
+require 'ImageUploader.php';
+
+$uploader = new \MyApp\ImageUploader();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $uploader->upload();
+}
 
 ?>
 <!DOCTYPE html>
@@ -17,7 +39,7 @@
 <body>
 
   <form action="" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="MAX_FILE_SIZE" value="">
+    <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo h(MAX_FILE_SIZE); ?>">
     <input type="file" name="image">
     <input type="submit" value="upload">
   </form>
